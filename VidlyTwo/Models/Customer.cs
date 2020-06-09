81fb77f5c11c522994de952e2a9df974654a6ddf
Javier Reyes <javier.reyes10@outlook.com>
2020-06-08 22:06:13 -0600

    Modified data base

--- VidlyTwo/Models/Customer.cs
+++ VidlyTwo/Models/Customer.cs
@@ -10,6 +10,9 @@ namespace VidlyTwo.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public bool IsSubscribedToNewsLetter { get; set; }
 
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
