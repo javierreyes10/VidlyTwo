fb002d3dac8beaefdb8ad1d799d0ec685d26dcce
Javier Reyes <javier.reyes10@outlook.com>
2020-06-13 10:41:31 -0600

    Adde validation message for customer name

--- VidlyTwo/Controllers/CustomersController.cs
+++ VidlyTwo/Controllers/CustomersController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -45,6 +45,17 @@ namespace VidlyTwo.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _customerService.MembershipTypes()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
             {
                 _customerService.Add(customer);
