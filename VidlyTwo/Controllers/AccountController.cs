5ccd956500497956d54895375d7a635b509a600a
Javier Reyes <javier.reyes10@outlook.com>
2020-06-18 22:05:35 -0600

    added driving license over registration model and view

--- VidlyTwo/Controllers/AccountController.cs
+++ VidlyTwo/Controllers/AccountController.cs
@@ -153,7 +153,7 @@ namespace VidlyTwo.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, DrivingLicense = model.DrivingLicense};
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
